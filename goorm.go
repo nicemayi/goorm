1405b7b49cd1ef008e4c28421941ce068bea462e
Zhe Wang <zhe.wang@instacart.com>
2020-12-24 14:21:14 -0800

    day 2

--- goorm.go
+++ goorm.go
@@ -2,12 +2,14 @@ package goorm
 
 import (
 	"database/sql"
+	"goorm/dialect"
 	"goorm/log"
 	"goorm/session"
 )
 
 type Engine struct {
-	db *sql.DB
+	db      *sql.DB
+	dialect dialect.Dialect
 }
 
 func NewEngine(driver, source string) (e *Engine, err error) {
@@ -22,7 +24,12 @@ func NewEngine(driver, source string) (e *Engine, err error) {
 		return
 	}
 
-	e = &Engine{db:db}
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Errorf("dialect %s Not Found", driver)
+		return
+	}
+	e = &Engine{db: db, dialect: dial}
 	log.Info("Connect database success")
 	return
 }
@@ -35,5 +42,5 @@ func (engine *Engine) Close() {
 }
 
 func (engine *Engine) NewSession() *session.Session {
-	return session.New(engine.db)
+	return session.New(engine.db, engine.dialect)
 }
