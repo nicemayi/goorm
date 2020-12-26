6b44485d43e22d6fb707d17d1ef31adeb9c17415
Zhe Wang <zhe.wang@instacart.com>
2020-12-25 22:14:21 -0800

    transaction

--- session/raw.go
+++ session/raw.go
@@ -12,10 +12,17 @@ import (
 type Session struct {
 	db       *sql.DB
 	sql      strings.Builder
+	tx       *sql.Tx
 	sqlVars  []interface{}
 	dialect  dialect.Dialect
 	refTable *schema.Schema
 	clause   clause.Clause
+}
+
+type CommonDB interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
 }
 
 func New(db *sql.DB, dialect dialect.Dialect) *Session {
@@ -25,20 +32,19 @@ func New(db *sql.DB, dialect dialect.Dialect) *Session {
 	}
 }
 
+var _ CommonDB = (*sql.DB)(nil)
+var _ CommonDB = (*sql.Tx)(nil)
+
 func (s *Session) Clear() {
 	s.sql.Reset()
 	s.sqlVars = nil
 	s.clause = clause.Clause{}
 }
-func (s *Session) DB() *sql.DB {
+func (s *Session) DB() CommonDB {
+	if s.tx != nil {
+		return s.tx
+	}
 	return s.db
-}
-
-func (s *Session) Raw(sql string, values ...interface{}) *Session {
-	s.sql.WriteString(sql)
-	s.sql.WriteString(" ")
-	s.sqlVars = append(s.sqlVars, values...)
-	return s
 }
 
 func (s *Session) Exec() (result sql.Result, err error) {
@@ -47,7 +53,6 @@ func (s *Session) Exec() (result sql.Result, err error) {
 	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
 		log.Error(err)
 	}
-
 	return
 }
 
@@ -64,4 +69,11 @@ func (s *Session) QueryRows() (rows *sql.Rows, err error) {
 		log.Error(err)
 	}
 	return
+}
+
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
 }
